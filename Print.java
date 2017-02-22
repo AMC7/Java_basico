a57e3c69fdfba801c3e94b71f0667ef5436c29d7
antonio <antoniomartinezcruz@ciencias.unam.mx>
2017-02-22 12:52:12 -0600

    Ya esta la version 3 falta documentar

--- Print.java
+++ Print.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
 public class Print{
    public static void p(Object o){
    System.out.println(o.toString());
+   }
+
+   public static void p(int i){
+   System.out.println(i);
+   }
+
+   public static void p(double i){
+   System.out.println(i);
+   }
+
+   public static void p(Object[]a){
+   System.out.println(Arrays.toString(a));
+   }
+
+   public static void p(int []a){
+   System.out.println(Arrays.toString(a));
+   }	
+
+   public static void p(double[]a){
+   System.out.println(Arrays.toString(a));
    }
 
 }
