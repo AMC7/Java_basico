b1bf718fe9e1436a4292f9ba3bf5dc03774b9a8d
AMC7 <antoniomartinezcruz@ciencias.unam.mx>
2017-03-29 09:56:55 -0600

    Añadi documentacion

--- util/Print.java
+++ util/Print.java
@@ -1,26 +1,28 @@
 package util;
 import java.util.Arrays;
+/**Clase para imprimir archivos*/
 public class Print{
+   /**Metodo estatico que imprime un objeto*/
    public static void p(Object o){
    System.out.println(o.toString());
    }
-
+   /**Metodo estatico que imprime un entero*/ 
    public static void p(int i){
    System.out.println(i);
    }
-
+   /**Metodo estatico que imprime un double*/
    public static void p(double i){
    System.out.println(i);
    }
-
+   /**Metodo estatico que imprime un arreglo de objetos*/
    public static void p(Object[]a){
    System.out.println(Arrays.toString(a));
    }
-
+   /**Metodo estatico que imprime un arreglo de ints*/
    public static void p(int []a){
    System.out.println(Arrays.toString(a));
    }	
-
+   /**Metodo estatico que imprime un arreglo de doubles*/
    public static void p(double[]a){
    System.out.println(Arrays.toString(a));
    }
