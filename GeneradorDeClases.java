27f700000efce4dbfe9db6150134fdd794a8aa22
AMC7 <antoniomartinezcruz@ciencias.unam.mx>
2017-04-03 17:15:01 -0600

    El autor lo corregi a Antonio Martinez

--- GeneradorDeClases.java
+++ GeneradorDeClases.java
@@ -10,7 +10,7 @@ public class GeneradorDeClases{
 	 String texto="import static util.Print.*;\n";
 	 texto+="import static util.ManejadorDeArchivos.*;\n";
 	 texto+="/**@version 1.0\n";
-	 texto+="   @author 1.0*/\n";
+	 texto+="   @author Antonio Martinez Cruz*/\n";
 	 texto+="public class "+nombreClase+"{\n\tpublic static void main(String [] args){\n\t}\n}\n";
 	 escribe(nombreClase+".java",texto);
 	 }catch(Exception e){}
